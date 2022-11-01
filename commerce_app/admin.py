f4f494f9bca5b960676d8cad7d3191e465c8ee5d
Abdullah OZER <abdullahozer11@hotmail.com>
2022-11-01 14:58:42 +0100

    Models are added to the admin page.

--- commerce_app/admin.py
+++ commerce_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from commerce_app.models import Client, Product, Vendor
+
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Vendor)
