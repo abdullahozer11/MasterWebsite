8979db095edf90456c698b3b310f585ce059a0fc
Abdullah OZER <abdullahozer11@hotmail.com>
2022-11-01 14:45:26 +0100

    New views are added.

--- commerce_app/urls.py
+++ commerce_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from commerce_app.views import IndexView
+from commerce_app.views import IndexView, AboutView, ContactView, ProductView, TestimonialView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('about/', AboutView.as_view(), name='about'),
-    path('', ContactView.as_view(), name='contact'),
-    path('', ProductView.as_view(), name='product'),
-    path('', TestimonialView.as_view(), name='testimonial'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('product/', ProductView.as_view(), name='product'),
+    path('testimonial/', TestimonialView.as_view(), name='testimonial'),
 ]
--- commerce_app/views.py
+++ commerce_app/views.py
@@ -5,3 +5,15 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "commerce_app/index.html"
+
+class AboutView(TemplateView):
+    template_name = "commerce_app/about.html"
+
+class ContactView(TemplateView):
+    template_name = "commerce_app/contact.html"
+
+class ProductView(TemplateView):
+    template_name = "commerce_app/product.html"
+
+class TestimonialView(TemplateView):
+    template_name = "commerce_app/testimonial.html"
