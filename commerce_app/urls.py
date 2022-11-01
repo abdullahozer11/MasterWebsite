275d12752b3372f4e46c542d260866a57567ce40
Abdullah OZER <abdullahozer11@hotmail.com>
2022-11-01 15:06:23 +0100

    Files are moved.

--- commerce_app/urls.py
+++ commerce_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from commerce_app.views import IndexView, AboutView, ContactView, ProductView, TestimonialView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
@@ -9,3 +10,5 @@ urlpatterns = [
     path('product/', ProductView.as_view(), name='product'),
     path('testimonial/', TestimonialView.as_view(), name='testimonial'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
