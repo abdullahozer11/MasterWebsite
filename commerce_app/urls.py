284577c5a16bd8f82c3e463908c0e26614da017f
Abdullah OZER <abdullahozer11@hotmail.com>
2022-11-01 14:28:23 +0100

    New files are added.

--- commerce_app/urls.py
+++ commerce_app/urls.py
@@ -4,4 +4,8 @@ from commerce_app.views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('', ContactView.as_view(), name='contact'),
+    path('', ProductView.as_view(), name='product'),
+    path('', TestimonialView.as_view(), name='testimonial'),
 ]
