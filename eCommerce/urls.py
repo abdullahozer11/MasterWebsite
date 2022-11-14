e1f5f861e45787d23f082fd1b10df9aa9fc954e8
Abdullah OZER <abdullahozer11@hotmail.com>
2022-11-14 23:55:21 +0100

    Portfolio page is added

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('commerce_app.urls')),
+    path('commerce/', include('commerce_app.urls')),
+    path('', TemplateView.as_view(template_name='portfolio/index.html')),
 ]
