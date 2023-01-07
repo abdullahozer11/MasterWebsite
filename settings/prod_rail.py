0b8562dee04b059a587d6060b80fc89d15f2615f
Abdullah OZER <abdullahozer11@hotmail.com>
2023-01-07 21:57:50 +0100

    database is customized to use postgresql with rails

--- settings/prod_rail.py
+++ settings/prod_rail.py
@@ -1,4 +1,7 @@
 """prod.py. Put your railway app production settings here."""
+import sys
+
+import dj_database_url
 
 from .base import *
 
@@ -7,12 +10,12 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", None)
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL") is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
-}
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
