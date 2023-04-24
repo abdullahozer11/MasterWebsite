861f89692340e927aabc886a72acf2f53f2674f0
Abdullah OZER <abdullahozer11@hotmail.com>
2023-04-24 21:55:35 +0200

    try except is added to TargetDate query

--- countdown/views.py
+++ countdown/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
 from countdown.models import TargetDate
 
@@ -14,7 +16,12 @@ class CountdownView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        target_date = TargetDate.objects.get(name=self.name)
+        try:
+            target_date = TargetDate.objects.get(name=self.name)
+        except TargetDate.DoesNotExist:
+            target_date = TargetDate(date=datetime(2023, 9, 7))
+        except Exception:
+            target_date = None
         if target_date:
             context['year'] = target_date.date.year
             context['month'] = target_date.date.month
