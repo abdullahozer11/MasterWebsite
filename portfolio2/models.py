df2acbc23f1bfbcad29efe484d1543ecb0918a6c
Abdullah OZER <abdullahozer11@hotmail.com>
2023-05-01 11:44:49 +0200

    add missing migration and add default field to image field of DemoApp

--- portfolio2/models.py
+++ portfolio2/models.py
@@ -24,7 +24,7 @@ class DemoApp(models.Model):
     desc = models.TextField(max_length=500)
     url = models.CharField(max_length=200)
     image = ResizedImageField(upload_to='portfolio2/app_images/', size=[500, 700], crop=['middle', 'center'],
-                              force_format="WEBP", quality=100, scale=1)
+                              force_format="WEBP", quality=100, scale=1, default='default.png')
 
     def __str__(self):
         return self.title
