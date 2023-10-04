c76c56c5bf04a681bd070c98fb4599fccfa94c20
Abdullah OZER <abdullahozer11@hotmail.com>
2023-10-04 20:18:59 +0200

    URLs are corrected to correctly catch urls asked from the server.

--- master_website/urls.py
+++ master_website/urls.py
@@ -11,8 +11,8 @@ from settings import local
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('todo_old/', include('todo_app.urls', namespace="todo")),
+    re_path('^admin/?', admin.site.urls),
+    re_path('^todo_old/?', include('todo_app.urls', namespace="todo")),
     re_path(r'^todo/?$', TemplateView.as_view(template_name='todo_react.html'), name="todo_react"),
     re_path(r'^words/?$', TemplateView.as_view(template_name='word_game.html'), name="word_game"),
     re_path(r'^pacman/?$', TemplateView.as_view(template_name='pacman/index.html'), name="pacman"),
@@ -23,14 +23,14 @@ urlpatterns = [
     re_path(r'^istanbul/?$', CountdownView.as_view(template_name='countdown/istanbul.html', name='Istanbul'), name="istanbul"),
     re_path(r'^athens/?$', CountdownView.as_view(template_name='countdown/athens.html', name='Athens'), name="athens"),
     re_path(r'^toronto/?$', CountdownView.as_view(template_name='countdown/toronto.html', name='Toronto'), name="toronto"),
-    path('commerce/', include('commerce_app.urls', namespace="commerce")),
-    path('words_api/', include('word_game_api.urls', namespace="words_api")),
-    re_path('', Portfolio2IndexView.as_view(), name="portfolio2"),
+    re_path(r'^commerce/?', include('commerce_app.urls', namespace="commerce")),
+    re_path(r'^words_api/?', include('word_game_api.urls', namespace="words_api")),
+    path('', Portfolio2IndexView.as_view(), name="portfolio2"),
     # captcha
-    path('captcha/', include('captcha.urls')),
+    re_path('^captcha/?', include('captcha.urls')),
     # allauth urls
-    path('accounts/', include('allauth.urls')),
-    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
+    re_path('^accounts/?', include('allauth.urls')),
+    re_path('^favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
     # "not found" URL
     re_path(r'^.*$', TemplateView.as_view(template_name='404.html'), name="404"),
 ]
